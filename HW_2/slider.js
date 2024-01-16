0d7dc7b9b7ae9872aee62c4efa5d1e3c012d3e38
ilyanik77 <110525890+ilyanik77@users.noreply.github.com>
2024-01-16 22:28:55 +0300

    Update slider.js

--- HW_2/slider.js
+++ HW_2/slider.js
@@ -30,11 +30,11 @@ function showNextSlide() {
 function updateSlider() {
     slides.forEach((slide, index) => {
     	if (index === slideIndex) {
-      		slide.style.display = "block";
+      	    slide.style.display = "block";
     	} else {
-    		slide.style.display = "none";
+    	    slide.style.display = "none";
     	}
-  	});
+    });
 }
 
 // Функция изменения кнопок навигации слайдера
@@ -44,11 +44,11 @@ function activeDot(index) {
 }
 
 dots.forEach((dot, index) => {
-  	dot.addEventListener("click", () => {
-    	slideIndex = index;
-    	updateSlider();
-    	activeDot(slideIndex);
-  	});
+    dot.addEventListener("click", () => {
+        slideIndex = index;
+        updateSlider();
+        activeDot(slideIndex);
+    });
 });
 
 
