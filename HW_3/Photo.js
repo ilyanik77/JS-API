08eef2b46a260e261dc9b7a0317db5dff4f85d48
ilya <ilyakhlamov77@gmail.com>
2024-01-20 00:18:36 +0300

    add .

---
+++ HW_3/Photo.js
@@ -0,0 +1,43 @@
+const photoContainer = document.getElementById("photo__container");
+let page = Math.floor(Math.random() * 100) + 1;
+
+async function fetchPhotos() {
+    try {
+        const response = await fetch(
+            `https://api.unsplash.com/photos?page=${page}&per_page=9&		
+            client_id=Ys1zvoO5KfCyOBFfUEWhTTvfbOZoZgsOj7ZpL_5pTew`
+        );
+
+        const photos = await response.json();
+        return photos;
+    } catch (error) {
+        console.error("Ошибка при загрузке фотографий:", error);
+        return [];
+    }
+}
+
+async function loadMorePhotos(page) {
+    const photos = await fetchPhotos();
+    const nameUser = photos[0].user.first_name;
+    const photo = photos[0].urls.small;
+    let likes = photos[0].likes;
+
+    const contentHtml = `<article>
+							<div class="">
+								<h2 class="title">Photo</h2>
+						  		<img src="${photo}" alt="photo">
+						  		<p class="nameUser">Фотограф: ${nameUser} </p>
+						  		<p id="likes">Лайки: ${likes}</p>
+							  	<button id="btnLikes">Нравится</button>
+							</div>
+						</article>`;
+    photoContainer.innerHTML = contentHtml;
+
+    btnLikes.addEventListener("click", () => {
+        const likesEl = document.getElementById("likes");
+        likes += 1;
+        likesEl.textContent = `Лайки: ${likes}`;
+    });
+}
+
+loadMorePhotos(page);
--- HW_3/loadPhoto.js
+++ HW_3/loadPhoto.js
@@ -1,26 +1,27 @@
 const photoContainer = document.getElementById("photo__container");
 let page = Math.floor(Math.random() * 100) + 1;
 
-async function fetchPhotos() {try {
-    	const response = await fetch(
-      	`https://api.unsplash.com/photos?page=${page}&per_page=9&		client_id=Ys1zvoO5KfCyOBFfUEWhTTvfbOZoZgsOj7ZpL_5pTew`);
+async function fetchPhotos() {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos?page=${page}&per_page=9&		client_id=Ys1zvoO5KfCyOBFfUEWhTTvfbOZoZgsOj7ZpL_5pTew`
+    );
 
-    	const photos = await response.json();
-    	return photos;
-  	} catch (error) {
-    	console.error("Ошибка при загрузке фотографий:", error);
-    	return [];
-  	}
+    const photos = await response.json();
+    return photos;
+  } catch (error) {
+    console.error("Ошибка при загрузке фотографий:", error);
+    return [];
+  }
 }
 
-
 async function loadMorePhotos(page) {
-  	const photos = await fetchPhotos();
-  	const nameUser = photos[0].user.first_name;
-  	const photo = photos[0].urls.small;
-  	let likes = photos[0].likes;
+  const photos = await fetchPhotos();
+  const nameUser = photos[0].user.first_name;
+  const photo = photos[0].urls.small;
+  let likes = photos[0].likes;
 
-  	const contentHtml = `<article>
+  const contentHtml = `<article>
 					  		<div class="">
 						  		<h2 class="title">Photo</h2>
 						  		<img src="${photo}" alt="photo">
@@ -29,13 +30,11 @@ async function loadMorePhotos(page) {
 							  	<button id="btnLikes">Нравится</button>
 							</div>
 					  	</article>`;
-  	photoContainer.innerHTML = contentHtml;
+  photoContainer.innerHTML = contentHtml;
 
-  	btnLikes.addEventListener("click", () => {
-    	const likesEl = document.getElementById("likes");
-    	likes += 1;
-    	likesEl.textContent = `Лайки: ${likes}`;
-  	});
+  btnLikes.addEventListener("click", () => {
+    const likesEl = document.getElementById("likes");
+    likes += 1;
+    likesEl.textContent = `Лайки: ${likes}`;
+  });
 }
-
-loadMorePhotos(page);
